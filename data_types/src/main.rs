427ad480f5482ef13a3eead7a97d12d642400bee
chishtil1730 <catterpi1432@gmail.com>
2026-02-05 14:58:51 +0530

    + data types and type casting in rust

---
+++ data_types/src/main.rs
@@ -0,0 +1,33 @@
+mod data_types;
+
+fn main() {
+    println!("From data types cargo");
+
+    let int =1;
+
+    let decimal = 23f64;
+
+    let name: &str = "Chishti";
+
+    let string: &str = &int.to_string();
+
+    for i in 0..2{
+        if i==0{
+            print!("Numero: {}, ",i+1);
+        }else {
+            print!("{}, ",i+1);
+        }
+    }
+    println!();
+
+    println!("Name: {}",name);
+
+    println!("Integer division {:.3}", 232f32/32f32);
+
+    println!("Float division: {} ", 1f32/2f32);
+
+    println!("Type casted into decimal: {}",decimal);
+
+    println!("{}", string);
+
+}
