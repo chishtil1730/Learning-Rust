48eb3166aff5ebf7c6cb2da28b652e52c68adbf2
chishtil1730 <catterpi1432@gmail.com>
2026-02-05 21:31:34 +0530

    +extracted top lines to method

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -4,32 +4,7 @@ mod arrays;
 fn main() {
     println!("From data types cargo");
 
-    let int =1;
-
-    let decimal = 23f64;
-
-    let name: &str = "Chishti";
-
-    let string: &str = &int.to_string();
-
-    for i in 0..2{
-        if i==0{
-            print!("Numero: {}, ",i+1);
-        }else {
-            print!("{}, ",i+1);
-        }
-    }
-    println!();
-
-    println!("Name: {}",name);
-
-    println!("Integer division {:.3}", 232f32/32f32);
-
-    println!("Float division: {} ", 1f32/2f32);
-
-    println!("Type casted into decimal: {}",decimal);
-
-    println!("{}", string);
+    basics_stuff();
 
     strings::strings();
 
@@ -39,4 +14,33 @@ fn main() {
 
     arrays::print_array(arr);
     println!("printed array:{:?} of length:{}",arr2,arr2.len());
+}
+
+fn basics_stuff() {
+    let int = 1;
+
+    let decimal = 23f64;
+
+    let name: &str = "Chishti";
+
+    let string: &str = &int.to_string();
+
+    for i in 0..2 {
+        if i == 0 {
+            print!("Numero: {}, ", i + 1);
+        } else {
+            print!("{}, ", i + 1);
+        }
+    }
+    println!();
+
+    println!("Name: {}", name);
+
+    println!("Integer division {:.3}", 232f32 / 32f32);
+
+    println!("Float division: {} ", 1f32 / 2f32);
+
+    println!("Type casted into decimal: {}", decimal);
+
+    println!("{}", string);
 }
